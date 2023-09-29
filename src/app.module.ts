19f53db99e7e7596c250e2df9cc9e847aaa111a4
PBHAHAHA <314709923@qq.com>
2023-09-29 09:31:06 +0800

    feat: add dabase config and core

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { database } from './config';
 import { ContentModule } from './modules/content/content.module';
+import { CoreModule } from './modules/core/core.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-    imports: [ContentModule],
+    imports: [ContentModule, CoreModule.forRoot(), DatabaseModule.forRoot(database)],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/modules/database/types.ts
@@ -0,0 +1,19 @@
+import { ObjectLiteral } from 'typeorm';
+
+export interface PaginateMeta {
+    itemCount: number;
+    totalItems?: number;
+    perPage: number;
+    totalPages?: number;
+    currentPage: number;
+}
+
+export interface PaginateOptions {
+    page: number;
+    limit: number;
+}
+
+export interface PaginateReturn<E extends ObjectLiteral> {
+    meta: PaginateMeta;
+    items: E[];
+}
