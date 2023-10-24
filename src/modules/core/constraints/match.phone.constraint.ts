4a448e363137bfbc9e545487a22699e07cc6468b
PBHAHAHA <314709923@qq.com>
2023-10-24 23:31:50 +0800

    feat:测试gitmerger

--- src/modules/core/constraints/match.phone.constraint.ts
+++ src/modules/core/constraints/match.phone.constraint.ts
@@ -14,6 +14,7 @@ export function isMatchPhone(
     if (!value) return false;
     const phoneArr: string[] = value.split('.');
     if (phoneArr.length !== 2) return false;
+    console.log(123);
     return isMobilePhone(phoneArr.join(''), locale, options);
 }
 
